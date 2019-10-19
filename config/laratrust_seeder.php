b4f11e1036b084b1f5f675f951b928920687f2a9
lotus-orb <syarief.yusuf@gmail.com>
2019-10-19 13:05:59 +0700

    Modify Auth Seeder

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -2,13 +2,9 @@
 
 return [
     'role_structure' => [
-        'superadministrator' => [
+        'admin' => [
             'users' => 'c,r,u,d',
             'acl' => 'c,r,u,d',
-            'profile' => 'r,u'
-        ],
-        'administrator' => [
-            'users' => 'c,r,u,d',
             'profile' => 'r,u'
         ],
         'user' => [
